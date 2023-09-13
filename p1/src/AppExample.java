dda4ea38c5834e76d75ad2de556dbe7507811126
aberraxao <diana_milu@live.com.pt>
2023-09-13 21:26:51 +0100

    Lab1 - imports updated

--- p1/src/AppExample.java
+++ p1/src/AppExample.java
@@ -1,7 +1,13 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class AppExample {
 
