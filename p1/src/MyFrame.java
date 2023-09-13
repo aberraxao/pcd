412740f4d4e747e8a32f51eeed9edc2ad14578c7
aberraxao <diana_milu@live.com.pt>
2023-09-13 22:05:24 +0100

    Lab1 - Ex1

---
+++ p1/src/MyFrame.java
@@ -0,0 +1,74 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MyFrame {
+    private JFrame frame;
+
+    public MyFrame(String title, int width, int height) {
+        frame = new JFrame(title);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        addFrameContent();
+
+        frame.setSize(width, height);
+
+        frame.pack();
+    }
+
+    public void open() {
+        frame.setVisible(true);
+    }
+
+    private void addFrameContent() {
+
+        frame.setLayout(new GridLayout(4, 1));
+
+        JTextField title = addPanel(frame, "title", "Hello");
+        JTextField width = addPanel(frame, "width", "300");
+        JTextField height = addPanel(frame, "height", "150");
+
+        addBottomPanel(frame, title, width, height);
+    }
+
+    private static JTextField addPanel(JFrame frame, String lb, String txt) {
+        JPanel pnTitle = new JPanel(new GridLayout(1, 2));
+        frame.add(pnTitle);
+        JLabel label = new JLabel(lb);
+        pnTitle.add(label);
+        JTextField text = new JTextField(txt);
+        pnTitle.add(text);
+        return text;
+    }
+
+    private static void addBottomPanel(JFrame frame, JTextField title, JTextField width, JTextField height) {
+        JPanel pnBottom = new JPanel(new GridLayout(1, 2));
+        frame.add(pnBottom);
+
+        JButton button = new JButton("update");
+        JCheckBox check = new JCheckBox("center");
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setTitle(title.getText());
+                frame.setSize(Integer.parseInt(width.getText()), Integer.parseInt(height.getText()));
+                if (check.isSelected())
+                    frame.setLocationRelativeTo(null);
+            }
+        });
+
+        pnBottom.add(button);
+        pnBottom.add(check);
+    }
+
+
+    public static void main(String[] args) {
+        MyFrame window = new MyFrame("Hello", 300, 150);
+        window.open();
+    }
+}
