3e8cf3a2fa87ddd17b56d02a312a9b8c3bb2049b
aberraxao <diana_milu@live.com.pt>
2023-09-27 20:28:06 +0100

    Add ex1_a static comments

--- p3/p3/src/ex1_a/Counter.java
+++ p3/p3/src/ex1_a/Counter.java
@@ -1,28 +1,43 @@
 package ex1_a;
 
 public class Counter {
-    private int count =0;
+    private int count = 0;
 
-    public int get(){
+    public int get() {
         return count;
     }
 
-    public void increment(){
+    public void increment() {
         // count = count + 1;
         // READ count
         // MODIFY add
         // WRITE count
-        count ++;
+        count++;
     }
 
-    private class ConcurrentCount extends Thread {
+    /*
+        private static class ConcurrentCount extends Thread {
+       private Counter shareadCounter;
+       public ConcurrentCount(Counter sharedCounter){
+           this.shareadCounter = sharedCounter;
+       }
 
         @Override
         public void run(){
             // TODO: 1000 incrementos no contador
+        }
+
+    }
+     */
+
+    private class ConcurrentCount extends Thread {
+
+        @Override
+        public void run() {
+            // TODO: 1000 incrementos no contador
             // Acesso ao contador "ex1_b.Counter.this"
 
-            for(int i = 0; i < 1000; i++){
+            for (int i = 0; i < 1000; i++) {
                 increment(); // same as ex1_b.Counter.this.increment();
             }
         }
@@ -31,19 +46,27 @@ public class Counter {
 
 
     public static void main(String[] args) throws InterruptedException {
+        /*
+        Counter sharedCounter = new Counter(); // Objeto partilhado
+        Counter.ConcurrentCount[] threads = new Counter.ConcurrentCount[4];
+        for (int i=0; i< threads.length; i++){
+            threads[i] = new Counter.ConcurrentCount(sharedCounter); // Partilha explícita
+        }
+        */
+
         Counter sharedCounter = new Counter(); // Objeto partilhado
         ConcurrentCount[] threads = new ConcurrentCount[4];
-        for (int i=0; i< threads.length; i++){
+        for (int i = 0; i < threads.length; i++) {
             threads[i] = sharedCounter.new ConcurrentCount(); // Partilha explícita
         }
 
         // Starts all threads
-        for (ConcurrentCount t: threads){
+        for (ConcurrentCount t : threads) {
             t.start();
         }
 
         // Waits for all threads to end
-        for (ConcurrentCount t: threads){
+        for (ConcurrentCount t : threads) {
             t.join();
         }
         // We expect counter <=4000, because we might lose some counts
