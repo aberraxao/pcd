eef94049ff5d813ce7a177f95df4ef2f5d1b5d67
aberraxao <diana_milu@live.com.pt>
2023-09-27 20:33:30 +0100

    Add ex1_b - synchronized

---
+++ p3/p3/src/ex1_b/Counter.java
@@ -0,0 +1,62 @@
+package ex1_b;
+
+public class Counter {
+    private int count = 0;
+
+    public int get() {
+        return count;
+    }
+
+    synchronized public void increment() {
+        // count = count + 1;
+        // READ count
+        // MODIFY add
+        // WRITE count
+        // synchronized (this) { // could have this here or add it before public
+        count++;
+        // }
+    }
+
+    synchronized public void incrementByValue(int val){
+       // synchronized (this){
+            count = count + val;
+      //  }
+    }
+
+    private class ConcurrentCount extends Thread {
+
+        @Override
+        public void run() {
+            // TODO: 1000 incrementos no contador
+            // Acesso ao contador "ex1_b.Counter.this"
+
+            for (int i = 0; i < 1000; i++) {
+                increment(); // same as ex1_b.Counter.this.increment();
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter sharedCounter = new Counter(); // Objeto partilhado
+        ConcurrentCount[] threads = new ConcurrentCount[4];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = sharedCounter.new ConcurrentCount(); // Partilha explícita
+        }
+
+        // Starts all threads
+        for (ConcurrentCount t : threads) {
+            t.start();
+        }
+
+        // Waits for all threads to end
+        for (ConcurrentCount t : threads) {
+            t.join();
+        }
+        // We expect counter <=4000, because we might lose some counts
+        System.out.println("ex1_b.Counter value: " + sharedCounter.get());
+    }
+
+}
+
